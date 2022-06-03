299f60e373146927cb5aa8d24587b8d32583244c
TatianaAraujo <71762581+TatianaAraujo@users.noreply.github.com>
2022-06-03 12:09:23 +0100

    dataware house updated

--- datawarehouse.sql
+++ datawarehouse.sql
@@ -4,44 +4,15 @@ CREATE DATABASE db_datawarehousing;
 USE db_datawarehousing;
 USE db_annp
 
-CREATE TABLE db_datawarehousing.result(
-    resultid        INT NOT NULL,
-    points          VARCHAR(25),
-    swimTime        VARCHAR(25),
-    reactionTime    VARCHAR(25),
-    status          VARCHAR(25),
-    /**EventId ??*/
-   
-    PRIMARY KEY (resultid) 
-);
-
-CREATE TABLE db_datawarehousing.split(
-    splitid         INT NOT NULL,
-    distance        VARCHAR(25),
-    swimTime        VARCHAR(25),
-    /*ResultId ??*/
-    
-    PRIMARY KEY (splitid) 
-);
-
 CREATE TABLE db_datawarehousing.swimstyle(
     swimstyleid     INT NOT NULL,
     distance        VARCHAR(25),
     stroke          VARCHAR(25),
     relaycount      VARCHAR(25),
-    /*EventId???*/
-    PRIMARY KEY (swimstyleid) 
+
+    PRIMARY KEY(swimstyleid) 
 );
 
-CREATE TABLE db_datawarehousing.event(
-    eventid         INT NOT NULL,
-    daytime         VARCHAR(25),
-    gender          VARCHAR(25),
-    `order`         VARCHAR(25),
-    number          VARCHAR(25),
-    /*SessionId??*/
-    PRIMARY KEY (eventid) 
-);
 
 CREATE TABLE db_datawarehousing.club(
     clubid          INT NOT NULL,
@@ -49,107 +20,67 @@ CREATE TABLE db_datawarehousing.club(
     name            VARCHAR(50),
     nation          VARCHAR(25),
     region          VARCHAR(25),
-    /*MeetId??*/
-    PRIMARY KEY (clubid) 
+    PRIMARY KEY(clubid) 
 );
 
-CREATE TABLE db_datawarehousing.session(
-    sessionid       INT NOT NULL,
-    date            VARCHAR(25),
-    name            VARCHAR(25),
-    number          VARCHAR(25),
-    warmUpTime      VARCHAR(25),
-    /*MeetId*/
-    PRIMARY KEY (sessionid) 
-);
 
 CREATE TABLE db_datawarehousing.athlete(
     athleteid       INT NOT NULL,
     completeName    VARCHAR(50),
     nation          VARCHAR(25),
-    age             INT,
+    birthdate       VARCHAR(25),
     license         VARCHAR(25),
-    /*....*/
-    PRIMARY KEY (athleteid) 
+    gender          VARCHAR(25),
+
+    PRIMARY KEY(athleteid) 
 );
 
 CREATE TABLE db_datawarehousing.meet(
     meetid          INT NOT NULL,
     city            VARCHAR(25),
-    /*
-    name            VARCHAR(25),
+    name            VARCHAR(75),
     organizer       VARCHAR(50),  
     number          VARCHAR(25),
-    */
-    PRIMARY KEY (meetid) 
+    dateStart       VARCHAR(25), /*vem do session */            
+    dateEnd         VARCHAR(25), /*vem do session */
+    course          VARCHAR(25), 
+    PRIMARY KEY(meetid) 
 );
-
-CREATE TABLE db_datawarehousing.pool(
-    poolid          INT NOT NULL,
-    /*.Sempre é preciso?.*/
-    PRIMARY KEY (poolid) 
-);
-
-
-
-INSERT INTO db_datawarehousing.result(resultid, points, swimTime, reactionTime, status) 
-    SELECT resultid, points, swimTime, reactionTime, status FROM db_annp.result;
-
-INSERT INTO db_datawarehousing.split(splitid, distance, swimTime) 
-    SELECT splitid, distance, swimTime FROM db_annp.split;
 
 INSERT INTO db_datawarehousing.swimstyle(swimstyleid, distance, stroke, relaycount) 
     SELECT swimstyleid, distance, stroke, relaycount FROM db_annp.swimstyle;
 
-INSERT INTO db_datawarehousing.event(eventid, daytime, gender, `order`, number) 
-    SELECT eventid, daytime, gender, `order`, number FROM db_annp.event;
-
 INSERT INTO db_datawarehousing.club(clubid, code, name, nation, region) 
     SELECT clubid, code, name, nation, region FROM db_annp.club;
 
-INSERT INTO db_datawarehousing.session(sessionid, date, name, number, warmUpTime) 
-    SELECT sessionid, date, name, number, timestampdiff(minute, timestamp(date,warmupfrom), timestamp(date,warmupuntil)) as warmUpTime FROM db_annp.session;    /*TODO Mudar o warmUp nesta linha*/
+INSERT INTO db_datawarehousing.athlete(athleteid, completeName, nation, birthdate, license,gender) 
+    SELECT athleteid, concat(firstname, ' ', lastname) as name, nation, birthdate , license, gender FROM db_annp.athlete;
 
-INSERT INTO db_datawarehousing.athlete(athleteid, completeName, nation, age, license) 
-    SELECT athleteid,  concat(firstname, ' ', lastname) as name, nation, year(from_days(to_days(now())-to_days(birthdate))) as age , license FROM db_annp.athlete;
+INSERT INTO db_datawarehousing.meet(meetid, city, name, organizer, number, course)
+    SELECT meetid, city, name, organizer, number, course FROM db_annp.meet;
 
-INSERT INTO db_datawarehousing.meet(meetid,city) /*TODO Confirmar se é suposto acrescentar mais coisas*/
-    SELECT meetid, city FROM db_annp.meet;
-
-INSERT INTO db_datawarehousing.pool(poolid) /*TODO Confirmar se é suposto acrescentar mais coisas*/
-    SELECT poolid FROM db_annp.pool;
-
-
-
+/* TODO: Preencher depois
+INSERT INTO db_datawarehousing.meet(meetid, dateStart, dateEnd) 
+    SELECT db_annp.meet.meetid, MIN(date), MAX(date) FROM db_annp.session, db_annp.meet where db_datawarehousing.meet.meetid = db_annp.session.meetid GROUP BY db_annp.session.meetid;*/
 
 CREATE TABLE db_datawarehousing.fact(
-    resultid    INT,   
-    splitid     INT,   
-    swimstyleid INT,   
-    eventid     INT,    
-    clubid      INT,   
-    sessionid   INT,    
     athleteid   INT,    
+    swimstyleid INT,   
+    clubid      INT,   
     meetid      INT,   
-    poolid      INT,
-    factid      INT NOT NULL AUTO_INCREMENT,
-    /*Acrescentar mais cenas*/
+    
+    swimTime    VARCHAR(25), /*Vem do result*/
+    points      VARCHAR(25), /*Vem do result*/
+    poolName    VARCHAR(50),
 
-    /*É necessário primary key??? Acho que sim...*/
-    PRIMARY KEY(factid)
-
-    /*FOREIGN KEY (resultid) REFERENCES db_datawarehousing.result(resultid),
-    FOREIGN KEY (splitid) REFERENCES db_datawarehousing.split(splitid),
-    FOREIGN KEY (swimstyleid) REFERENCES db_datawarehousing.swimstyle(swimstyleid),
-    FOREIGN KEY (eventid) REFERENCES db_datawarehousing.event(eventid),
-    FOREIGN KEY (clubid) REFERENCES db_datawarehousing.club(clubid),
-    FOREIGN KEY (sessionid) REFERENCES db_datawarehousing.session(sessionid),
     FOREIGN KEY (athleteid) REFERENCES db_datawarehousing.athlete(athleteid),
-    FOREIGN KEY (meetid) REFERENCES db_datawarehousing.meet(meetid),
-    FOREIGN KEY (poolid) REFERENCES db_datawarehousing.pool(poolid)*/
+    FOREIGN KEY (swimstyleid) REFERENCES db_datawarehousing.swimstyle(swimstyleid),
+    FOREIGN KEY (clubid) REFERENCES db_datawarehousing.club(clubid),
+    FOREIGN KEY (meetid) REFERENCES db_datawarehousing.meet(meetid)
 );
 
+INSERT INTO db_datawarehousing.fact(athleteid, clubid) 
+    SELECT athlete.athleteid, club.clubid FROM db_annp.athlete, db_annp.club WHERE athlete.clubid = club.clubid;
 
-
-INSERT INTO db_datawarehousing.fact(splitid) 
-    SELECT splitid FROM db_annp.split;
+INSERT INTO db_datawarehousing.fact(points, swimTime)
+    SELECT result.points, result.swimTime FROM db_annp.result WHERE db_annp.result.resultid = db_datawarehousing.athlete.athleteid
